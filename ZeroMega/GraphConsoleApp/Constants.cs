805a7768a0d5dae3862570af0c25af0cbec598fb
Allan Holanda Targino <allan_targino@hotmail.com>
2016-08-04 16:12:56 -0300

    Graph Console

---
+++ ZeroMega/GraphConsoleApp/Constants.cs
@@ -0,0 +1,29 @@
+﻿//----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+namespace GraphConsoleApp
+{
+    internal class Constants
+    {
+        public const string TenantName = "targino.onMicrosoft.com";
+        public const string TenantId = "c504a843-931f-477a-9b4d-010218a8a9c6";
+        public const string ClientId = "091c133e-b393-4bf9-9002-59a2814b1cad";
+        public const string ClientSecret = "b7bBZMk8B3F0WYj4u7SGOzVyX4g4EgtSRk18k99fjns=";
+        public const string ClientIdForUserAuthn = "66133929-66a4-4edc-aaee-13b04b03207d";
+        public const string AuthString = "https://login.windows.net/" + TenantName;
+        public const string ResourceUrl = "https://graph.windows.net";
+    }
+}
