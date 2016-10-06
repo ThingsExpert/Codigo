33bf946060b4687586706bc122e6d5c5d3285b8a
Allan Targino <allan_targino@hotmail.com>
2016-10-06 00:37:49 -0300

    remove personal data

--- Bash Scripts/SendDataToAzure.sh
+++ Bash Scripts/SendDataToAzure.sh
@@ -5,10 +5,10 @@ alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.ar
 alias computehash='python -c "import sys, hmac, hashlib, base64; bytes_msg = str.encode(sys.argv[1] + '"'"'\n'"'"' + sys.argv[2]); bytes_key = str.encode(sys.argv[3]);  dig = hmac.new(bytes_key, msg=bytes_msg, digestmod=hashlib.sha256).digest();print(base64.b64encode(dig).decode());"'
 
 #Parameters
-Endpoint="https://eventhuballan-ns.servicebus.windows.net/"
-EndpointMsg="$Endpoint/eventhuballan/messages"
+Endpoint="<INSERT HERE YOUR AZURE EVENT HUB ENDPOINT>"
+EndpointMsg="$Endpoint/<YOUR EVENT HUB NAME>/messages"
 SasKeyName="all"
-SasKeyValue="y4hHJ6E25apmgbBz7/dSN7aXraJPPDELrGS0SCycFYA="
+SasKeyValue="<YOUR EVENT HUB KEY>"
 Authorization="SharedAccessSignature"
 Duration=3600
 
